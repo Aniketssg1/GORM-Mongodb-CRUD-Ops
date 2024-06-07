dc8061f543b994285e35b5295457083247b16b02
aniketga <aniketga@mkcl.org>
2024-06-07 11:33:28 +0530

    Initial

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Product struct {
+	Code  string
+	Price float64
+}
+
+func Connect(uri string) (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to MongoDb")
+	return client, nil
+}
+
+func main() {
+	_, err := Connect("mongodb://localhost:27017")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
